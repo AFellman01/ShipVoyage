d62b430feaa8fa52bf37e76c25b8a6e8bab81cf9
Alexander Fellman <alexanderfellman@Alexanders-MacBook-Pro.local>
2017-11-01 14:41:53 -0400

    lll

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,22 @@
 // jQuery way
-$(document).ready(function() {
-
-      // 
+    $(document).ready(function() {
+      //
       // $("#PostBtn")on("click", function() {
       //        alert('button clicked');
       //      }
 
 
-           $(".PostBtn").on('click', function(){
-       		    alert('click');
-            )];
+           $('.post').on('click', function (){
+location.href = ("https://tools.usps.com/go/TrackConfirmAction?tLabels=" + "")
+           });
+
+
+           $('.parcel').on('click', function (){
+location.href = (`"https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums"` + "")
+  });
+
+
+          $('.fedex').on('click', function (){
+location.href = ("https://www.fedex.com/apps/fedextrack/?tracknumbers" + "")
+  });
+});
