01af3b70baa06093d4ee4a415fd7da3d0acf178c
Alexander Fellman <alexanderfellman@Alexanders-MacBook-Pro.local>
2017-11-01 13:49:14 -0400

    jquery

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// jQuery way
+$(document).ready(function() {
+
+      // 
+      // $("#PostBtn")on("click", function() {
+      //        alert('button clicked');
+      //      }
+
+
+           $(".PostBtn").on('click', function(){
+       		    alert('click');
+            )];
