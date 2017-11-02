f60088b42990f3a0a478b8c4c0b151c77e15ad5c
Alexander Fellman <alexanderfellman@Alexanders-MacBook-Pro.local>
2017-11-02 10:11:49 -0400

    Latest?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ location.href = ("https://tools.usps.com/go/TrackConfirmAction?tLabels=" + "")
 
 
            $('.parcel').on('click', function (){
-location.href = (`"https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums"` + "")
+location.href = ("https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=" + "")
   });
 
 
