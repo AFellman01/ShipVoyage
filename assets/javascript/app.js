b5c789ace743bd3f1633efaf41df279dd6d93a85
Alexander Fellman <alexanderfellman@Alexanders-MacBook-Pro.local>
2017-11-02 10:31:54 -0400

    semi-functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,10 +4,13 @@
       // $("#PostBtn")on("click", function() {
       //        alert('button clicked');
       //      }
+var trackNums = function() {
+document.getElementById('#track').value;
 
+};
 
            $('.post').on('click', function (){
-location.href = ("https://tools.usps.com/go/TrackConfirmAction?tLabels=" + "")
+location.href = ("https://tools.usps.com/go/TrackConfirmAction?tLabels=" + trackNums)
            });
 
 
