3dc729e99fe630587ee158472f7bad67d0ea5fd1
Alexander Fellman <alexanderfellman@Alexanders-MacBook-Pro.local>
2017-11-03 11:42:24 -0400

    Practical

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,43 +10,52 @@
     $('.post').on('click', function (){
       var tracknum = $('#track').val();
       // var regex = new RegExp('^\\d+$');
-
+console.log(tracknum.length)
 
       // first conditional tests for tracking numbers beginning with E
       if (tracknum[0] === 'E') {
         console.log("USPS")
           window.open("https://tools.usps.com/go/TrackConfirmAction?tLabels=" + tracknum, '_blank');
-  }
-      else if (tracknum[1] == 'Z'){
+        }
+      else if (tracknum[1] === 'Z'){
         console.log("UPS")
         window.open("https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=" + tracknum, '_blank');
       }
+      else if (tracknum[0] === 'T'){
+        console.log("Amazon")
+        alert("Please Enter Valid Tracking Number")
+      }
       else if (typeof(parseInt(tracknum,0)) === 'number') {
-      console.log("FedEX")
+      console.log("Number")
+
+      if (tracknum.length === 10){
+      console.log("DHL")
+window.open("http://www.dhl.com/en/express/tracking.html?AWB=" + tracknum, '_blank')
+      }
+      else if (tracknum.length === 12){
+      console.log("fedex")
       window.open("https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=" + tracknum, '_blank')
       }
-      else {
-        console.log("Help!");
-      }
+}
+
+});
 
 
+      // }
+      //
+      // if (localStorage){
+      //   $('.post').on('click', function (){
+      //   sessionStorage.setItem(tracknum, "carrier");
+      //   document.getElementById("result").innerHTML = "You have clicked the button " +
+      //   sessionStorage.clickcount + " time(s) in this session.";
+      //
+      //     // Code for localStorage/sessionStorage.
+      //
+      //
+      // } else {
+      //     // Sorry! No Web Storage support..
+      // }
 
 
-    });//.post onclick
-
-//
-//
-//            $('.parcel').on('click', function (){
-//              var tracker = $('#track').val()
-//
-//
-//   });
-//
-//
-//           $('.fedex').on('click', function (){
-//             var tracker = $('#track').val()
-//
-//   });
-// });
 
 }); //document.ready
