703d4db4e52b256ae345aa02098e3c306756bad1
Alexander Fellman <alexanderfellman@Alexanders-MacBook-Pro.local>
2017-11-03 10:22:07 -0400

    WorkingScript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,36 +11,25 @@
       var tracknum = $('#track').val();
       // var regex = new RegExp('^\\d+$');
 
-      if (tracknum[0] === 'E') {
-        console.log("Yo");
-      }
-      // first conditional tests for tracking numbers beginning with E
 
-      //   //if the tracking number begins with E, then check if the next letter is A
-      //   if (tracknum[1] === 'A'){
-      //     console.log("A");
-      //     window.open("https://tools.usps.com/go/TrackConfirmAction?tLabels=" + tracknum, '_blank');
-      //   }
-      //   //if not A, then check if it's C
-      //   else if (tracknum[1] === "C") {
-      //     console.log("international");
-      //     window.open("https://tools.usps.com/go/TrackConfirmAction?tLabels=" + tracknum, '_blank');
-      //   }
-      //   //if not A or C, then it's not a valid USPS tracknum
-      //   else {
-      //     console.log("please enter a valid tracking number");
-      //   }
+      // first conditional tests for tracking numbers beginning with E
+      if (tracknum[0] === 'E') {
+        console.log("USPS")
+          window.open("https://tools.usps.com/go/TrackConfirmAction?tLabels=" + tracknum, '_blank');
+  }
       else if (tracknum[1] == 'Z'){
+        console.log("UPS")
         window.open("https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=" + tracknum, '_blank');
       }
       else if (typeof(parseInt(tracknum,0)) === 'number') {
-        console.log("Fuck yeah!");
+      console.log("FedEX")
+      window.open("https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=" + tracknum, '_blank')
       }
       else {
         console.log("Help!");
       }
 
-// location.href = ("https://tools.usps.com/go/TrackConfirmAction?tLabels=" + tracker )
+
 
 
     });//.post onclick
@@ -56,7 +45,7 @@
 //
 //           $('.fedex').on('click', function (){
 //             var tracker = $('#track').val()
-// window.open("https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=" + tracker), '_blank'
+//
 //   });
 // });
 
