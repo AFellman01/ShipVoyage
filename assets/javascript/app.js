499fec0ed0cdb85fea94b19b4ae532328cefe0cc
Alexander Fellman <alexanderfellman@Alexanders-MacBook-Pro.local>
2017-11-03 13:55:07 -0400

    LocalStorage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,46 @@
-// jQuery way
-    $(document).ready(function() {
+$(document).ready(function() {
+  var tracknum;
 
-      // var tracker = $('#track').val();
-      // var res = tracker.match  ('[0-9]{4} [0-9]{4} [0-9]{2}');
+  $(".post").on("click", function() {
+    tracknum = $("#track").val();
 
+    if (tracknum[0] === "E") {
+      console.log("USPS")
+      window.open("https://tools.usps.com/go/TrackConfirmAction?tLabels=" + tracknum, "_blank");
+      if (localStorage) {
+        document.getElementById("postStoreInfo").innerHTML = ("Tracking Number " +
+          tracknum + " is from the Post Office.")
+      }
+    } else if (tracknum[1] === "Z") {
+      console.log("UPS")
+      window.open("https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=" + tracknum, "_blank");
+      if (localStorage) {
+        document.getElementById("upsStoreInfo").innerHTML = ("Tracking Number " +
+          tracknum + " is from UPS" )
+      }
+    } else if (tracknum[0] === "T") {
+      console.log("Amazon")
+      if (localStorage) {
+        document.getElementById("errorInfo").innerHTML = ("Tracking Number " +
+          tracknum + " is from Amazon. Go to their site to view it. ")
+      }
+    } else if (typeof(parseInt(tracknum, 0)) === "number") {
 
-
-
-    $('.post').on('click', function (){
-      var tracknum = $('#track').val();
-      // var regex = new RegExp('^\\d+$');
-console.log(tracknum.length)
-
-      // first conditional tests for tracking numbers beginning with E
-      if (tracknum[0] === 'E') {
-        console.log("USPS")
-          window.open("https://tools.usps.com/go/TrackConfirmAction?tLabels=" + tracknum, '_blank');
+      if (tracknum.length === 10) {
+        console.log("DHL")
+        window.open("http://www.dhl.com/en/express/tracking.html?AWB=" + tracknum, "_blank")
+        if (localStorage) {
+          document.getElementById("dhlStoreInfo").innerHTML = ("Tracking Number " +
+            tracknum + " is from DHL ")
         }
-      else if (tracknum[1] === 'Z'){
-        console.log("UPS")
-        window.open("https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums=" + tracknum, '_blank');
+      } else if (tracknum.length === 12) {
+        console.log("fedex")
+        window.open("https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=" + tracknum, "_blank")
+        if (localStorage) {
+          document.getElementById("fedexStoreInfo").innerHTML = ("Tracking Number " +
+            tracknum + " is from FedEX ")
+        }
       }
-      else if (tracknum[0] === 'T'){
-        console.log("Amazon")
-        alert("Please Enter Valid Tracking Number")
-      }
-      else if (typeof(parseInt(tracknum,0)) === 'number') {
-      console.log("Number")
-
-      if (tracknum.length === 10){
-      console.log("DHL")
-window.open("http://www.dhl.com/en/express/tracking.html?AWB=" + tracknum, '_blank')
-      }
-      else if (tracknum.length === 12){
-      console.log("fedex")
-      window.open("https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers=" + tracknum, '_blank')
-      }
-}
-
+    }
+  });
 });
-
-      // 
-      // }
-      //
-      // if (localStorage){
-      //   $('.post').on('click', function (){
-      //   sessionStorage.setItem(tracknum, "carrier");
-      //   document.getElementById("result").innerHTML = "You have clicked the button " +
-      //   sessionStorage.clickcount + " time(s) in this session.";
-      //
-      //     // Code for localStorage/sessionStorage.
-      //
-      //
-      // } else {
-      //     // Sorry! No Web Storage support..
-      // }
-
-
-
-}); //document.ready
